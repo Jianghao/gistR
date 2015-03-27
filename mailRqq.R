dabbb6e806afcfb154f77eb4585de64188a7e704
Jianghao Wang <wangjh@lreis.ac.cn>
2015-03-27 11:06:42 +0800

    Update mailRqq.R

--- mailRqq.R
+++ mailRqq.R
@@ -1,12 +1,12 @@
-if("mailR" %in% rownames(installed.packages()) == FALSE) install.packages("mailR")
-library(mailR)
+# if("mailR" %in% rownames(installed.packages()) == FALSE) install.packages("mailR")
+# library(mailR)
 send.mail.qq <- function(subject, passwd, body="Hello World"){
-  mailR::send.mail(from = "hellowangjh@qq.com",
-            to = "hellowangjh@qq.com",
+  send.mail(from = "240798582@qq.com",
+            to = "240798582@qq.com",
             subject = subject,
             body = body,
             smtp = list(host.name = "smtp.qq.com", port = 465, 
-                        user.name = "hellowangjh@qq.com", passwd = passwd, ssl = TRUE),
+                        user.name = "240798582@qq.com", passwd = passwd, ssl = TRUE),
             authenticate = TRUE,
             send = TRUE)
   return(NULL)
