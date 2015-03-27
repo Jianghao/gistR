08300cfe9dc112c3549a8cdd0c9988c88eb7a4f5
Jianghao Wang <wangjh@lreis.ac.cn>
2015-03-27 11:39:38 +0800

    Update mailR163.R

--- mailR163.R
+++ mailR163.R
@@ -1,9 +1,14 @@
 # using 163 because it does not need authorize code
-send.mail(from = "sendmailR@163.com",
-          to = "240798582@qq.com",
-          subject = subject,
-          body = body,
-          smtp = list(host.name = "smtp.163.com", port = 465, 
-                      user.name = "sendmailR@163.com", passwd = "wang123456", ssl = TRUE),
-          authenticate = TRUE,
-          send = TRUE)
+# if("mailR" %in% rownames(installed.packages()) == FALSE) install.packages("mailR")
+# library(mailR)
+mailR163 <- function(subject, body="Hello World"){
+  send.mail(from = "sendmailR@163.com",
+            to = "240798582@qq.com",
+            subject = subject,
+            body = body,
+            smtp = list(host.name = "smtp.163.com", port = 465, 
+                        user.name = "sendmailR@163.com", passwd = "wang123456", ssl = TRUE),
+            authenticate = TRUE,
+            send = TRUE)
+  return(NULL)
+}
